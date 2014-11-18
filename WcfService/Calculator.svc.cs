dcecd454055129690935e828c6fe78fed2ecf4d2
Hitendra Boda <hitendra.boda@interactive.in>
2014-11-18 15:08:46 +0530

    Added summary to service

--- WcfService/Calculator.svc.cs
+++ WcfService/Calculator.svc.cs
@@ -16,25 +16,42 @@ namespace WcfService
         /// </summary>
         /// <param name="number1">The number1.</param>
         /// <param name="number2">The number2.</param>
-        /// <returns></returns>
+        /// <returns>Adds result</returns>
         public double AddNumbers(double number1, double number2)
         {
             double result = number1 + number2;
             return result;
         }
 
+        /// <summary>
+        /// Substracts the numbers.
+        /// </summary>
+        /// <param name="number1">The number1.</param>
+        /// <param name="number2">The number2.</param>
+        /// <returns>Substracts result</returns>
         public double SubstractNumbers(double number1, double number2)
         {
             double result = number1 - number2;
             return result;
         }
 
+        /// <summary>
+        /// Multiplies the numbers.
+        /// </summary>
+        /// <param name="calculatorModel">The calculator model.</param>
+        /// <returns>Multiplies result</returns>
         public double MultiplyNumbers(CalculatorModel calculatorModel)
         {
             double result = calculatorModel.number1 * calculatorModel.number2;
             return result;
         }
 
+        /// <summary>
+        /// Divisions the numbers.
+        /// </summary>
+        /// <param name="number1">The number1.</param>
+        /// <param name="number2">The number2.</param>
+        /// <returns>Divisions result</returns>
         public double DivisionNumbers(double number1, double number2)
         {
             double result = number1 / number2;
